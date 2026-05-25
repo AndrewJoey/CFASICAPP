f1efba2d84f1f0e48237b943f5775a4307234c5f
AndrewJoey <Joeyandrewzhou@gmail.com>
2026-05-25 13:29:48 +0800

    Initial Commit

---
+++ CFASICAPP/CFASICAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFASICAPPApp.swift
+//  CFASICAPP
+//
+//  Created by 周安骏 on 2026/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CFASICAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CFASICAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CFASICAPP
+//
+//  Created by 周安骏 on 2026/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
